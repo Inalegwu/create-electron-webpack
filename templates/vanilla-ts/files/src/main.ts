8c1f6669012cd41ddc0fabc2747273bdb1431c7d
sprout2000 <wassabie64@gmail.com>
2022-10-27 18:20:43 +0900

    perf: Updated templates for vanilla & vanilla-ts.

--- templates/vanilla-ts/files/src/main.ts
+++ templates/vanilla-ts/files/src/main.ts
@@ -8,34 +8,15 @@ if (process.env.NODE_ENV === 'development') {
   });
 }
 
-function createWindow() {
-  // Create the browser window.
+app.whenReady().then(() => {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
     },
   });
 
-  // and load the index.html of the app.
   mainWindow.loadFile('dist/index.html');
-
-  // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
-  createWindow();
+  mainWindow.webContents.openDevTools({ mode: 'detach' });
 });
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function () {
-  app.quit();
-});
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+app.on('window-all-closed', () => app.quit());
