08e37626e78bd9ca11c0f8595e4121673d819397
sprout2000 <wassabie64@gmail.com>
2022-11-26 09:05:49 +0900

    perf: Updated Vue3 templates.

--- templates/vue-ts/files/src/web/App.vue
+++ templates/vue-ts/files/src/web/App.vue
@@ -1,19 +1,14 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+const count = ref(0);
+</script>
+
 <template>
   <div class="container">
     <h1>{{ count }}</h1>
     <button @click="count++">Count</button>
   </div>
 </template>
-
-<script lang="ts">
-export default {
-  data() {
-    return {
-      count: 0,
-    };
-  },
-};
-</script>
 
 <style>
 .container {
--- templates/vue/files/src/web/App.vue
+++ templates/vue/files/src/web/App.vue
@@ -1,19 +1,14 @@
+<script setup>
+import { ref } from 'vue';
+const count = ref(0);
+</script>
+
 <template>
   <div class="container">
     <h1>{{ count }}</h1>
     <button @click="count++">Count</button>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      count: 0,
-    };
-  },
-};
-</script>
 
 <style scoped>
 .container {
