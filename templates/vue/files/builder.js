366efda64ae5a9faabe645140fd3bb576129dec5
sprout2000 <wassabie64@gmail.com>
2023-03-10 10:29:21 +0900

    feat: Add electron-builder to tempaltes.

---
+++ templates/vue/files/builder.js
@@ -0,0 +1,28 @@
+require("electron-builder").build({
+  config: {
+    appId: "com.Electron.ElectronApp",
+    productName: "Electron App",
+    artifactName: "${productName}-${version}-${platform}-${arch}.${ext}",
+    directories: {
+      output: "release",
+    },
+    files: [
+      "dist/**/*",
+      "!node_modules/@babel/core",
+      "!node_modules/@babel/preset-env",
+      "!node_modules/babel-loader",
+      "!node_modules/cross-env",
+      "!node_modules/css-loader",
+      "!node_modules/electron-builder",
+      "!node_modules/electronmon",
+      "!node_modules/html-webpack-plugin",
+      "!node_modules/mini-css-extract-plugin",
+      "!node_modules/npm-run-all",
+      "!node_modules/rimraf",
+      "!node_modules/vue-loader",
+      "!node_modules/wait-on",
+      "!node_modules/webpack",
+      "!node_modules/webpack-cli",
+    ],
+  },
+});
